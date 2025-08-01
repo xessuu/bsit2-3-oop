45b952b727a9eca962ccb816ddc166f87c15a4ac
xessuu <pbisrael07645@liceo.edu.ph>
2025-08-01 17:37:51 +0800

    hello my nigga

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,38 @@
-// Book.java
-class Book {
-    // Attributes (public by default, no private keyword)
-    String title;
-    String author;
-    int pages;
-    boolean isAvailable;
+import java.util.Scanner;
 
-    // Constructor
-    public Book(String title, String author, int pages) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isAvailable = true; // Set to true by default
-
-        // Print message when new book is added
-        System.out.println("New book '" + title + "' by " + author + " has been added to the library!");
-    }
-
-    // Method to display book information
-    public void displayInfo() {
-        System.out.println("Book Information ");
-        System.out.println(" Title: " + title);
-        System.out.println(" Author: " + author);
-        System.out.println(" Pages: " + pages);
-        System.out.println(" Available: " + (isAvailable ? "Yes" : "No"));
-        System.out.println("");
-    }
-
-    // Method to borrow a book
-    public void borrowBook() {
-        isAvailable = false;
-        System.out.println(" Book Borrowed");
-    }
-
-    // Method to return a book
-    public void returnBook() {
-        isAvailable = true;
-        System.out.println(" Book Returned ");
-    }
-}
-
-// Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Library Management System ");
+        Scanner scanner = new Scanner(System.in);
+        int totalSum = 0;
+        int numberOfInputs = 5;
+        int[] numbers = new int[numberOfInputs];
 
-        // Create three Book objects as required
-        Book book1 = new Book("The Cotton Picker ", "Arnold SchwachNigger", 180);
-        Book book2 = new Book("The Slavery", "Harold the Slave", 281);
-        Book book3 = new Book("The Book of Whips(1800's)", "Demetreus Scalliwag", 328);
+        System.out.println("Welcome to the Number Total Program!");
 
-        System.out.println(); // Empty line for better formatting
 
-        // Display information for all books
-        System.out.println("Displaying all books in the library:");
-        book1.displayInfo();
-        book2.displayInfo();
-        book3.displayInfo();
+        for (int i = 0; i < numberOfInputs; i++) {
+            boolean validInput = false;
 
-        // Test borrowing and returning functionality
-        System.out.println("Testing borrow/return functionality:");
-        System.out.println(" Borrowing books ");
-        book1.borrowBook();
-        book2.borrowBook();
+            while (!validInput) {
+                System.out.print("Enter number " + (i + 1) + ": ");
+                try {
+                    numbers[i] = Integer.parseInt(scanner.nextLine());
+                    totalSum += numbers[i];
+                    validInput = true; // Break out of the loop if input is valid
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input! Please enter a valid integer.");
+                }
+            }
+        }
 
-        System.out.println(" Checking availability after borrowing ");
-        book1.displayInfo();
-        book2.displayInfo();
 
-        System.out.println(" Returning books ");
-        book1.returnBook();
+        System.out.println("\nYou entered the following numbers:");
+        for (int i = 0; i < numberOfInputs; i++) {
+            System.out.println("Number " + (i + 1) + ": " + numbers[i]);
+        }
 
-        System.out.println(" Final status ");
-        book1.displayInfo();
-        book2.displayInfo();
-        book3.displayInfo();
+        System.out.println("\nTotal Sum: " + totalSum);
+
+        scanner.close();
     }
-
 }
