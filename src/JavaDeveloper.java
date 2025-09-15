8beb2571542a189edd121aae49c49269fa0edf7b
Paul Benedict Israel <pbisrael07645@liceo.edu.ph>
2025-09-15 15:08:26 +0800

    Create JavaDeveloper.java

---
+++ src/JavaDeveloper.java
@@ -0,0 +1,6 @@
+public class JavaDeveloper extends Developer {
+    public JavaDeveloper(String name, int employeeId, double baseSalary, String department,
+                         boolean fullTime, int projectsCompleted) {
+        super(name, employeeId, baseSalary, department, fullTime, "Java", projectsCompleted);
+    }
+}
