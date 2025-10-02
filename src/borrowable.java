e3717a0f8c98ded994c256160c8abcacaa72e6e3
Paul Benedict Israel <pbisrael07645@liceo.edu.ph>
2025-10-02 15:40:47 +0800

    Create borrowable.java

---
+++ src/borrowable.java
@@ -0,0 +1,9 @@
+public interface Borrowable {
+  void borrowItem(String borrowerName);
+  void returnItem();
+  boolean isAvailable();
+  int getBorrowingPeriod();
+default String getBorrowingStatus() {
+  return isAvailable() ? "Available for borrowing" : "Currently borrowed"; }
+  }
+}
